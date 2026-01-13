36a75b9b05185b6303517c6b0a83c632104d5d74
Sarjan Shah <your-email@gmail.com>
2026-01-14 01:43:34 +0530

    Disable analytics to allow deployment without environment variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,11 +59,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={[inter.variable, archivoBlack.variable, "font-display"].join(" ")} suppressHydrationWarning>
       <head>
-        <Script
+        {/* <Script
           defer
           src={process.env.UMAMI_DOMAIN}
           data-website-id={process.env.UMAMI_SITE_ID}
-        ></Script>
+        ></Script> */}
         {/* <Analytics /> */}
       </head>
       <body>
