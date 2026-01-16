c50ff18c11ccdb218d6ecc5411254837fa082803
Sarjan Shah <your-email@gmail.com>
2026-01-17 02:29:08 +0530

    style: increase container margins to p-4 for comfortable spacing

--- src/components/sections/achievements.tsx
+++ src/components/sections/achievements.tsx
@@ -23,7 +23,7 @@ const AchievementsSection = () => {
                             <div className="absolute inset-0 bg-gradient-to-r from-black/20 to-transparent z-10" />
 
                             {/* Image Grid */}
-                            <div className="grid grid-cols-1 grid-rows-2 h-full gap-2 p-2 bg-muted/20">
+                            <div className="grid grid-cols-1 grid-rows-2 h-full gap-4 p-4 bg-muted/20">
                                 <div className="relative rounded-lg overflow-hidden w-full h-full bg-black/5">
                                     <Image
                                         src="/assets/hackathon/hack1.jpg"
@@ -44,7 +44,7 @@ const AchievementsSection = () => {
                         </div>
 
                         {/* Content Side - Takes 3/5 (60%) space */}
-                        <CardContent className="px-6 pt-6 pb-2 lg:p-10 flex flex-col justify-center relative lg:col-span-3">
+                        <CardContent className="px-6 pt-6 pb-4 lg:p-10 flex flex-col justify-center relative lg:col-span-3">
                             <div className="absolute top-0 right-0 p-6 opacity-10">
                                 <Trophy size={120} />
                             </div>
